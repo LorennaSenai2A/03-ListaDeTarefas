7483ee46b761745ff4d4102be6a0ba6a448003b1
Aluna.Lorena <lorennasenaia@gmail.com>
2025-08-27 15:14:50 -0300

    Implementar recursos de gerenciamento de tarefas com AsyncStorage para armazenamento persistente

--- App.js
+++ App.js
@@ -1,141 +1,248 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  FlatList,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [tasks,setTasks] = useState([]); //Estado para armazenar a lista de tarefas
+  const [newTask,setNewTask] = useState(""); //Estado para o texto da nova tarefa
+
+useEffect(() => {
+  const loadTasks = async () => {
+try{
+const saveTasks = await AsyncStorage.getItem("taks");
+}catch(error){
+  console.error("Errp ao carregar as tarefas:", error);
+}
+};
+loadTasks();
+}, []);
+
+useEffect(() =>{
+  const saveTasks = async () => {
+try{
+  await AsyncStorage.setItem("tasks",JSON.stringify(tasks));
+} catch (error) {
+console.error("Erro ao salvar tarefas:", error);
+}
+};
+  saveTasks();
+}, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim().length > 0){
+      //Garante que a tarefa não seja vazia
+      setTasks((prevTasks)=>[
+        ...prevTasks,
+        {id: Date.now().toString(), text: newTask.trim(), completed: false }, //Cria uma nova tarefa com id unico
+      ]);
+      setNewTask(""); //Limpar o campo de input
+      Keyboard.dismiss(); //Fecha o teclado do usuario
+    }else{
+      Alert.alert("Atenção", "Por favor, digite uma tarefa.");
+    }
+  };
+
+  const toggleTaskCompleted = (id) => {
+    setTasks((prevTasks)=>
+      prevTasks.map((taks) =>
+        taks.id === id ? { ...taks, completed: !taks.completed } : taks
+  )
+  );
+  };
+
+  const deleteTaks = (id) => {
+    Alert.alert(
+      "Confirmar exclusão",
+      "Tem certeza que deseja excluir esta tarefa?",
+      [
+        {text: "Cancelar", style:"cancel"},
+        {text: "Excluir", style: "destructive", 
+onPress: () =>
+  setTasks((prevTasks) => prevTasks.filter((task) => task.id 
+!== id)),
+        },
+      ]
+    );
+  };
+
+   const renderList = ({item}) => (
+  <View style={styles.taskItem} key={item.id}>
+  <TouchableOpacity style={styles.taskTextContainer}>
+    <Text 
+    style={[styles.taskText, item.completed && styles.completedTaskItem]}>
+      {item.text}
+      </Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity onPress={() => deleteTaks(item.id)}>
+    <Text style={styles.taskText}>🆇</Text>
+  </TouchableOpacity>
+  </View>
+  );
+
   return (
     <View style={styles.container}>
-      {/*Cabeçalho */}
       <View style={styles.topBar}>
         <Text style={styles.topBarTitle}>Minhas Tarefas</Text>
         <TouchableOpacity>
-          <Text>🌛</Text>
+          <Text>🌙</Text>
         </TouchableOpacity>
       </View>
-{/*Localiza usuario */}
-<View style={styles.card}>
-  <TextInput 
-  style={styles.input}
-placeholder="Adicionar nova tarefa..."
-  />
-  <TouchableOpacity style={styles.addButton}>
-    <Text style={styles.buttonText}>Adicionar</Text>
-  </TouchableOpacity>
-</View>
 
-{/*Lista de tarefas do usuario */}
-<FlatList
-style={styles.flatList} 
-ListEmptyComponent={() => (
-<Text style={styles.emptyListText}>
-  Nenhuma tarefa adicionada ainda
-  </Text>
-)}
-contentContainerStyle={styles.flatListContent}
-/>
-      <StatusBar style="auto" />
+      {/* Local onde o usuario insere as tarefas */}
+      <View style={styles.card}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicionar nova tarefa..."
+          value={newTask}
+          onChangeText={setNewTask}
+          onSubmitEditing={addTask} //Adiciona a tarefa ao pressionar enter no teclado
+        />
+        <TouchableOpacity style={styles.addButton} onPress={addTask}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Lista de tarefas do usuario */}
+      <FlatList
+        style={styles.flatList}
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={renderList}
+        //renderItem={({ item }) => (
+         // <View key={item.id} style={styles.taskItem}>
+           // <Text>{item.text}</Text>
+           // <TouchableOpacity>
+           // <Text>🆇</Text>
+           // </TouchableOpacity>
+         // </View>
+       // )}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyListText}>
+            Nenhuma tarefa adicionada ainda.
+          </Text>
+        )}
+        contentContainerStyle={styles.flatListContent}
+      />
+      
+
+      <StatusBar style="auto" /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#e0f7fa",
     flex: 1,
-    backgroundColor: '#fff',
   },
-topBar:{
-  backgroundColor: "#e0f7fa" ,
-flexDirection:"row",
-justifyContent:"space-between",
-alignItems:"center",
-paddingHorizontal: 20,
-paddingTop:50,
-paddingBottom: 15,
-borderBottomWidth: 1,
-borderBottomColor: "rgba(0,0,0,0.1)"
-},
-topBarTitle: {
-  color: "#00796b",
-  fontSize: 24,
-  fontWeight: "bold",
-},
-card: {
-  shadowColor: "#000",
-  margin: 20,
-borderRadius: 15,
-padding: 20,
-shadowOffset: {width: 0, height: 4},
-shadowOpacity: 0.2,
-shadowRadius: 8,
-elevation: 10,
-shadowColor: "#000",
-},
-input:{
-  backgroundColor: "#fcfcfc",
-  color: "#333",
-  borderColor: "#b0bec5",
-  borderWidth: 1,
-  borderRadius: 15,
-  padding: 20,
-  fontSize: 18,
-  marginBottom: 10,
-},
-addButton:{
-  backgroundColor: "#009688",
-  paddingVertical: 12,
-  borderRadius: 10,
-  alignItems: "center",
-},
-buttonText: {
-//  color: "#fff",
-  fontSize: 18,
-  fontWeight: "bold",
-},
-flatListContent: {
-  paddingBottom: 10,
-},
-taskItem:{
-  backgroundColor: "#fff",
-  color: "#333",
-  borderWidth: 1,
-  borderColor: "rgba(0,0,0,0.)",
-  flexDirection: "row",
-  justifyContent:"space-between",
-  alignItems: "center",
-  borderRadius: 10,
-  padding: 15,
-  marginVertical: 10,
-  marginHorizontal: 15,
-  shadowOffset: {width: 0, height: 2},
-shadowOpacity: 0.1,
-shadowRadius: 3,
-elevation: 5,
-},
-taskTextContainer:{
-  flex: 1,
-  marginRight: 10,
-},
-taskText:{
-  color: "#333",
-  fontSize: 18,
-  flexWrap: "wrap"
-},
-completedTaskItem:{
-  textDecorationLine: "line-through",
-  opacity: 0.6,
-},
-deleteButton: {
-  padding: 8,
-  borderRadius: 5,
-},
-deleteButton:{
-  //color: "#fff"
-  fontSize: 22,
-  fontWeight: "bold",
-},
-emptyListText:{
-  color: "#",
-  textAlign: "center",
-  marginTop: 50,
-  fontSize: 16,
-},
+  topBar: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingTop: 50, //Ajuste para a barra de status
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0,0,0,0.1)",
+  },
+  topBarTitle: {
+    color: "#00796b",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: "#fff",
+    color: "#000",
+    shadowColor: "#000",
+    margin: 20,
+    borderRadius: 15,
+    padding: 20,
+    shadowOffset: { widht: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 10, //elevar a cima dos outros
+  },
+  input: {
+    backgroundColor: "#fcfcfc",
+    color: "#333",
+    borderColor: "#b0bec5",
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 20,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  addButton: {
+    backgroundColor: "#009688",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  flatListContent: {
+    paddingBottom: 10, //Espaçamento no final da lista
+  },
+  taskItem: {
+    backgroundColor: "#fff",
+    borderColor: "rgba(0,0,0,0.1)",
+    color: "#333",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    marginHorizontal: 15,
+    shadowOffset: { widht: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+    borderWidth: 1,
+  },
+  taskTextContainer:{
+    flex:1, //Permite que o texto ocupe o espaço disponivel
+    marginRight: 10,
+  },
+  taskText:{
+    color: "#333",
+    fontSize: 18,
+    flexWrap: "wrap", //permite que o texto quebre linha 
+  },
+  completedTaskItem:{
+    textDecorationLine: "line-through", //risca o texto
+    opacity: 0.6,
+  },
+  deleteButton: {
+    padding: 8,
+    borderRadius: 5,
+  },
+  deleteButtonText:{
+    // color: "#fff"
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  emptyListText:{
+    color: "#9e9e9e",
+    textAlign: "center",
+    marginTop: 50,
+    fontSize: 16,
+  },
 });
+
+
